b3033db3a31787747ae970059da0b59dbded8777
Arikaim <info@arikaim.com>
2023-07-20 16:01:55 +0300

    =

--- view/components/api/api.js
+++ view/components/api/api.js
@@ -5,6 +5,13 @@ function AddressApi() {
     this.update = function(formId, onSuccess, onError) {
         return arikaim.put('/api/address/update',formId,onSuccess,onError);          
     };
+
+    this.updateMap = function(latitude, longitude, onSuccess, onError) {
+        return arikaim.put('/api/address/map/update',{
+            latitude: latitude,
+            longitude: longitude
+        },onSuccess,onError);          
+    };
 }
 
 var addressApi = new AddressApi();
